5ae2d5dc6762e329e458f6b17d0a30190214327a
Aryan R <aryan.rajalbandi.github@outlook.com>
2020-07-15 15:49:04 +0530

    This the second commit

---
+++ sample/app/src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hi I am Aryan. This is the second commit in the github");
+    }
+}
